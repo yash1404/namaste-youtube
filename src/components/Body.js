96bf4a0957d7981302522a8b8dc00bf18351702f
yash singh <yashe045@gmail.com>
2023-08-02 17:02:54 +0530

    showing videos in our app

--- src/components/Body.js
+++ src/components/Body.js
@@ -4,7 +4,7 @@ import MainContainer from './MainContainer'
 
 const Body = () => {
   return (
-    <div className="flex">
+    <div className="grid grid-cols-12">
     <Sidebar/>
     <MainContainer/>
     </div>
--- src/components/VideoCart.js
+++ src/components/VideoCart.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
+import { kFormatter } from "../utils/Kformatter";
 
-const VideoCart = () => {
+const VideoCart = (props) => {
+  const { videoInfo } = props;
   return (
-    <div>VideoCart</div>
-  )
-}
+    <React.Fragment>
+      <div className="flex flex-wrap p-4">
+        {videoInfo.map((curr, ind) => {
+          const { url } = curr.snippet.thumbnails.high;
+          const { title, channelTitle } = curr.snippet;
+          const { viewCount } = curr.statistics;
+          return (
+            <div key={curr.id} className="w-64 mt-5 ml-2 shadow-lg p-4 cursor-pointer">
+              <img src={url} alt="thumbnails" className="rounded-lg"/>
+              <p className="font-bold">{title}</p>
+              <p className="font-semibold">{channelTitle}</p>
+              <span>{kFormatter(viewCount)} Views</span>
+            </div>
+          );
+        })}
+      </div>
+    </React.Fragment>
+  );
+};
 
-export default VideoCart
+export default VideoCart;

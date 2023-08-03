49685b939abfa7369905c51691e7bfee2f10becb
yash singh <yashe045@gmail.com>
2023-08-03 19:40:48 +0530

    add comment sction to page

---
+++ src/components/Comment.js
@@ -0,0 +1,26 @@
+import React from "react";
+import user from "../assest/Images/profile-user.png";
+const Comment = ({ commentData }) => {
+  return (
+    <React.Fragment>
+      {commentData.map((commentdatas, ind) => {
+        console.log("commentdata", commentdatas);
+        const { name, text } = commentdatas;
+        return (
+          <div
+            key={ind}
+            className="flex items-center mt-2 p-2 bg-gray-200 rounded-lg w-full"
+          >
+            <img className="w-8 h-8 mr-2" src={user} alt="comment user" />
+            <div>
+              <p className="font-semibold capitalize">{name}</p>
+              <p>{text}</p>
+            </div>
+          </div>
+        );
+      })}
+    </React.Fragment>
+  );
+};
+
+export default Comment;
